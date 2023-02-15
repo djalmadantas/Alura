785a69a3a4bc06d09d7ed012f3f5c7a8e1cb76b8
DJ <djalma.dantas@gmail.com>
2023-02-15 10:48:58 -0300

    Primeiro Commit

---
+++ MicroServicos/src/main/java/com/alura/micro/servicos/Main.java
@@ -0,0 +1,17 @@
+package com.alura.micro.servicos;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
